950c37b0adfea945caecb925006d5ea40a34622f
Glitch (hello-express) <none>
2021-03-16 08:57:22 +0000

    🚣🍝 Checkpoint ./commands/erkek.js:28281870/17 ./commands/kadın.js:28281870/19 ./arwene.js:28355691/36

--- commands/erkek.js
+++ commands/erkek.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, message, args) => {
     const etiketlenenKişi = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 if(!etiketlenenKişi) return message.channel.send(`${client.emojis.cache.get(ayarlar.no)} **Kaydetmek için bir kişi etiketlemelisin!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
 
-if(!etiketlenenKişi.user.username.includes(ayarlar.tag) && !etiketlenenKişi.user.discriminator.includes("1281") && !etiketlenenKişi.roles.cache.has(ayarlar.vipRol) && !etiketlenenKişi.roles.cache.has(ayarlar.boosterRol)) return message.channel.send(`${client.emojis.cache.get(ayarlar.no)} **Belirtilen kişinin kaydı \`booster değil/vip değil/tag almamış\` durumlarından herhangi birinden dolayı gerçekleştirilemedi!**`).then(message.react(client.emojis.cache.get(ayarlar.no))) 
+if(!etiketlenenKişi.user.username.includes("Shex") && !etiketlenenKişi.user.discriminator.includes("1281") && !etiketlenenKişi.roles.cache.has(ayarlar.vipRol) && !etiketlenenKişi.roles.cache.has(ayarlar.boosterRol)) return message.channel.send(`${client.emojis.cache.get(ayarlar.no)} **Belirtilen kişinin kaydı \`booster değil/vip değil/tag almamış\` durumlarından herhangi birinden dolayı gerçekleştirilemedi!**`).then(message.react(client.emojis.cache.get(ayarlar.no))) 
   
 const isim = args.splice(1).join(' ')
 if(!isim) return message.channel.send(`${client.emojis.cache.get(ayarlar.no)} **Kaydetmek için bir isim belirtmelisin!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
