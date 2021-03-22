73051140d2ce6f9dc688764faeeb69958a3fba47
Glitch (hello-express) <none>
2021-03-22 06:02:16 +0000

    ⚽️🍼 Checkpoint ./commands/isim.js:29018071/4111

--- commands/isim.js
+++ commands/isim.js
@@ -3,16 +3,16 @@ const db = require("quick.db");
 const ayarlar = require("../ayarlar.json")
 
 module.exports.run = async (client, message, args) => {
-    if(ayarlar.yetkiliRol.some(arwene => message.member.roles.cache.has(arwene)) && !message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`${client.guild.emojis.cache.get(ayarlar.no)} **Bu işlemi kullanmak için gerekli yetkin yok!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
+    if(!ayarlar.yetkiliRol.some(arwww => message.member.roles.cache.has(arwww)) && !message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`${client.emojis.cache.get(ayarlar.no)} **Bu işlemi gerçekleştirmek için gerekli yetkin yok!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
 
-    const etiketlenenKişi = message.mentions.members.first() || message.guild.members.cache.get(args[1]);
-if(!etiketlenenKişi) return message.channel.send(`${client.guild.emojis.cache.get(ayarlar.no)} **Kaydetmek için bir kişi etiketlemelisin!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
+    const etiketlenenKişi = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+if(!etiketlenenKişi) return message.channel.send(`${client.emojis.cache.get(ayarlar.no)} **İsim değiştirmek için bir kişi etiketlemelisin!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
 
 const isim = args.splice(1).join(' ')
 const yaş = args[2]
-if(!isim) return message.channel.send(`${client.guild.emojis.cache.get(ayarlar.no)} **Kaydetmek için bir isim belirtmelisin!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
-if(!yaş) return message.channel.send(`${client.guild.emojis.cache.get(ayarlar.no)} **Kaydetmek için bir yaş belirtmelisin!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
-if(isNaN(yaş)) return message.channel.send(`${client.guild.emojis.cache.get(ayarlar.no)} **Belirttiğin yaş rakamlardan oluşmalı!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
+if(!isim) return message.channel.send(`${client.emojis.cache.get(ayarlar.no)} **İsim değiştirmek için bir isim belirtmelisin!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
+if(!yaş) return message.channel.send(`${client.emojis.cache.get(ayarlar.no)} **İsim değiştirmek için için bir yaş belirtmelisin!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
+if(isNaN(yaş)) return message.channel.send(`${client.emojis.cache.get(ayarlar.no)} **Belirttiğin yaş rakamlardan oluşmalı!**`).then(message.react(client.emojis.cache.get(ayarlar.no)))
 
 etiketlenenKişi.setNickname(`${ayarlar.tag} ${isim} ${ayarlar.sembol} ${yaş}`)
 
@@ -20,18 +20,18 @@ message.react(client.emojis.cache.get(ayarlar.yes))
 
 const arwEmbed = new Discord.MessageEmbed()
 .setColor("RANDOM")
-.setDescription(`Kullanıcının ismi ${ayarlar.tag} ${isim} ${ayarlar.sembol} ${yaş} olarak değiştirildi!`)
+.setDescription(`Kullanıcının ismi \`${ayarlar.tag} ${isim} ${ayarlar.sembol} ${yaş}\` olarak değiştirildi!`)
 .setFooter(ayarlar.footer)
 .setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true}))
 .setTimestamp()
 
 message.channel.send(arwEmbed)
 
-db.push(`isimler.${etiketlenenKişi.id}`, `
-İsim: isim
-Yaş: yaş
-Yetkili: message.author
-`)
+db.push(`isimler.${etiketlenenKişi.id}`, {
+İsim: isim,
+Yaş: yaş,
+Yetkili: message.author.id
+})
 
 }
 exports.config = {
