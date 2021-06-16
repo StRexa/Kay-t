b4c4aa2d06d1aeb3b57e575a46bf345ceafa55ed
Glitch (hello-express) <none>
2021-06-16 16:59:00 +0300

    ilk gönderme

--- matthe.js
+++ matthe.js
@@ -1,9 +1,9 @@
-// Sorununz olursa Matthe#0001 ulaşınız. https://discord.gg/vAKkqEqgfE
+// Sorununz olursa Matthe#0001 ulaşınız.
 
 const Discord = require("discord.js")
 const client = new Discord.Client()
 const ayarlar = require("./ayarlar.json")
-const moment = require("moment")//Matthe#0001
+const moment = require("moment")//Matthe#1000
 const fs = require("fs")
 const db = require("quick.db")
 const chalk = require("chalk")
@@ -32,7 +32,7 @@ client.on('message', async message => {
   })
 
 client.on("ready", () => {
-    console.log(chalk.redBright(`tm`))
+    console.log(chalk.redBright(`Botun aktif Özgürcüm`))
 })
 
 // BOTUN İNTENTLERİNİ AÇMAYI UNUTMAYIN 
@@ -61,8 +61,8 @@ client.on("guildMemberAdd", member => {
       const kurulus = new Date().getTime() - user.createdAt.getTime();  
      const gecen = moment.duration(kurulus).format(` YY **[Yıl]** DD **[Gün]** HH **[Saat]** mm **[Dakika,]**`) 
     var kontrol;
-  if (kurulus < 1296000000) kontrol = `Ve senin hesabın sunucumuza kayıt olmak için daha çok genç! :x: `
-  if (kurulus > 1296000000) kontrol = `Ve senin hesabın sunucumuza kayıt olmak için tüm şartları karşılıyor! :ballot_box_with_check: `
+  if (kurulus < 1296000000) kontrol = `Ve senin hesabın sunucumuza kayıt olmak için daha çok genç! ❌ `
+  if (kurulus > 1296000000) kontrol = `Ve senin hesabın sunucumuza kayıt olmak için tüm şartları karşılıyor! ☑️ `
     moment.locale("tr");
   
 member.roles.add(ayarlar.kayıtsızRol)
@@ -80,47 +80,10 @@ Ayrıca hesabın 15 günden fazla bir süredir Discord'da bulunmalı.
     
 Ceza işlemlerin <#KURALLAR KANALI ID> kanalını okuduğun varsayılarak uygulanır. ( <@&HOS GELDİN MESAJ YETKİLİ ROL ID> )`)});
 
-client.login(process.env.TOKEN)
+
+client.login(ayarlar.token)
 
 client.on("ready", () => {
   client.channels.cache.get(ayarlar.botSesKanal).join();
   });
 //Youtube Matthe
-//----------------------------------------------------- TAG ROL ------------------------------------------------\\
-
-// tag rol kodu bana ait değildir, geliştirip sizlere sundum.
-client.on("userUpdate", async function(oldUser, newUser) { 
-    const guildID = "847733452643368990"// sunucu ıd
-    const roleID = "847741789019963394"// taglı rolünüzün ıd
-    const tag = "Force"// tagınız
-    const chat = '847743477516075009'// chat kanalı ıd
-    const taglog = '847744584075444226' // log kanalı ıd
-  
-    const guild = client.guilds.cache.get(guildID)
-    const role = guild.roles.cache.find(roleInfo => roleInfo.id === roleID)
-    const member = guild.members.cache.get(newUser.id)
-    const embed = new Discord.MessageEmbed().setAuthor(member.displayName, member.user.avatarURL({ dynamic: true })).setColor('#ff0010').setTimestamp().setFooter('Matthe was here!');
-    if (newUser.username !== oldUser.username) {
-        if (oldUser.username.includes(tag) && !newUser.username.includes(tag)) {
-            member.roles.remove(roleID)
-            client.channels.cache.get(taglog).send(embed.setDescription(`${newUser} Kullanıcısı tagımızı çıkardığı için taglı rolü alındı!`))
-        } else if (!oldUser.username.includes(tag) && newUser.username.includes(tag)) {
-            member.roles.add(roleID)
-            client.channels.cache.get(chat).send(`**Mükemmel! ${newUser} Tagımızı alarak ailemize katıldı!**`)
-            client.channels.cache.get(taglog).send(embed.setDescription(`${newUser} Kullanıcısı tagımızı aldığı için taglı rolü verildi!`))
-        }
-    }
-   if (newUser.discriminator !== oldUser.discriminator) {
-        if (oldUser.discriminator == "1110" && newUser.discriminator !== "1110") {
-            member.roles.remove(roleID)
-            client.channels.cache.get(taglog).send(embed.setDescription(`${newUser} Kullanıcısı etiket tagımızı çıkardığı için taglı rolü alındı!`))
-        } else if (oldUser.discriminator !== "1110" && newUser.discriminator == "1110") {
-            member.roles.add(roleID)-
-            client.channels.cache.get(taglog).send(embed.setDescription(`${newUser} Kullanıcısı etiket tagımızı aldığı için taglı rolü verildi!`))
-            client.channels.cache.get(chat).send(`**Mükemmel! ${newUser} Etiket tagımızı alarak ailemize katıldı!**`)
-        }
-    }
-  
-  })
-
-//----------------------------------------------------- TAG ROL ------------------------------------------------\\
